fa805403d5983c2eeef825f99e8eb690f5c41c3b
Mikhail Gunin <gunka462@gmail.com>
2020-06-20 14:27:55 +0300

    Initial commit.

---
+++ cpp_lab1/README.md
@@ -0,0 +1,15 @@
+12б: Преобразовать заданную строку следующим образом: повторить каждый символ, который отличается от заданного символа.
+
+```
+lab1.exe
+
+This is a text that is very suitable for testing and debugging the ReplaceDelete application (sentence0). You can write some commands that will replace and delete some of the words (sentence1).
+
+t
+```
+
+=>
+
+```
+TThhiiss  iiss  aa  teexxt  thhaat  iiss  vveerryy  ssuuiitaabbllee  ffoorr  teesstiinngg  thhee  RReeppllaacceeDDeelleetee  aapppplliiccaatiioonn  ((ss00))..  YYoouu  ccaann  wwrriitee  ssoommee  ccoommmmaannddss  thhaat  wwiillll  rreeppllaaccee  aanndd  ddeelleetee  ssoommee  ooff  thhee  wwoorrddss  ((ss11))..
+```
---
+++ cpp_lab1/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+struct State {
+    char nonRepeatedCharacter;
+    char text[1000];
+};
+
+
+unsigned strlen(char text[])
+{
+    unsigned size = 0;
+    while (text[size]) { size++; };
+    return size;
+}
+
+
+void duplicateWithShifting(char text[], int index)
+{
+    auto len = strlen(text);
+
+    auto shiftedLength = len - index;
+
+    char* src = text + index;
+    char* dest = src + 1;
+
+    memmove(dest, src, shiftedLength + 1);
+
+    text[index + 1] = text[index];
+}
+
+
+int main(int argc, char* argv[])
+{
+    State state;
+
+    cout << "Enter text: " << endl;
+    cin.getline(state.text, sizeof(state.text));
+
+    cout << "Enter the symbol that won't be repeated: " << endl;
+    cin >> state.nonRepeatedCharacter;
+
+    for (unsigned i = 0; i < strlen(state.text); i++)
+    {
+        if (state.text[i] != state.nonRepeatedCharacter)
+        {
+            duplicateWithShifting(state.text, i);
+            i++;
+        }
+    }
+
+    cout << state.text;
+}
